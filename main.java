846f5af51914ac040538a672418ca843b4e9927e
alexisanchezz <2022110521@udh.edu.pe>
2024-09-11 15:13:21 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class main 
+{
+    public static void main(String[] args)
+    {
+        main resultado = new main();
+        resultado.saludo();
+    }
+    public void saludo (){
+        system.out.printLn("Metodo que no retorna nada.")
+    }
+    
+}
